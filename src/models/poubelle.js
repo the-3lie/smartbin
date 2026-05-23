82af04fb4165276a140698be763a33f9c7887d72
the-3lie <elyseeprunelle.com>
2026-05-23 21:33:17 +0200

    le premier

--- src/models/poubelle.js
+++ src/models/poubelle.js
@@ -1,25 +1,29 @@
 const mongoose = require("mongoose");
 
 const poubelleSchema = new mongoose.Schema({
-  rfid_uid: {
-    type: String,
-    unique: true
-  },
-  nom: String,
-  latitude: Number,
-  longitude: Number,
-  niveau: {
-    type: Number,
-    default: 0
-  },
-  statut: {
-    type: String,
-    default: "vide"
-  },
-  active: {
-    type: Boolean,
-    default: false
-  }
+
+    nom: {
+        type: String,
+        required: true
+    },
+
+    uid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    niveau: {
+        type: Number,
+        default: 0
+    },
+
+    statut: {
+        type: String,
+        enum: ["vide", "en cours", "pleine", "panne"],
+        default: "vide"
+    }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("Poubelle", poubelleSchema);
