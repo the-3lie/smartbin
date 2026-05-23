4cb60dd6dd164de1dd0edee2ec9d6f11ba2fd7b3
the-3lie <elyseeprunelle.com>
2026-05-23 21:34:42 +0200

    le premier

--- src/routes/poubelleRoutes.js
+++ src/routes/poubelleRoutes.js
@@ -1,9 +1,19 @@
-const router = require("express").Router();
+const express = require("express");
+
+const router = express.Router();
+
 const controller = require("../controllers/poubelleController");
 
+router.post("/rfid", controller.rfid);
+
+router.post("/niveau", controller.niveau);
+
 router.get("/", controller.getPoubelles);
+
 router.post("/", controller.createPoubelle);
+
 router.put("/:id", controller.updatePoubelle);
+
 router.delete("/:id", controller.deletePoubelle);
 
 module.exports = router;
