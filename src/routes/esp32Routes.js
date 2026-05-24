7a31753b7812f820fdf7bbddeec6aa016f21a7ac
the-3lie <elyseeprunelle.com>
2026-05-24 12:17:59 +0200

    le premier

--- src/routes/esp32Routes.js
+++ src/routes/esp32Routes.js
@@ -1,7 +1,7 @@
-// const router = require("express").Router();
-// const controller = require("../controllers/esp32Controller");
+const router = require("express").Router();
+const controller = require("../controllers/esp32Controller");
 
-// router.post("/rfid", controller.rfid);
-// router.post("/niveau", controller.niveau);
+router.post("/rfid", controller.rfid);
+router.post("/niveau", controller.niveau);
 
-// module.exports = router;
+module.exports = router;
