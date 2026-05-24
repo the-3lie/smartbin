2f3f74fb15e0ae6f1b3063bc0013d96473b8d0a1
the-3lie <elyseeprunelle.com>
2026-05-24 13:31:32 +0200

    le premier

--- src/app.js
+++ src/app.js
@@ -65,20 +65,17 @@ app.get("/", (req, res) => {
 
 // ================= MONGODB =================
 
-mongoose.connect(process.env.MONGO_URI)
-
-.then(() => {
-
-    console.log("MongoDB connecté");
-
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-
+.then(() => {
+    console.log("✅ MongoDB connecté");
+})
 .catch((err) => {
-
+    console.log("❌ Erreur MongoDB");
     console.log(err);
-
 });
-
 
 // ================= 404 =================
 
